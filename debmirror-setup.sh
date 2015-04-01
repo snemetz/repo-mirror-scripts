2522065b99b763afe4388fb3f0a5a2838742b1be
Steven Nemetz <snemetz@hotmail.com>
2015-04-01 16:00:18 -0700

    Add 2 more package dependences

--- debmirror-setup.sh
+++ debmirror-setup.sh
@@ -7,7 +7,7 @@
 debmirror_version='2.16'
 tarfile="debmirror_${debmirror_version}ubuntu1.tar.gz"
 
-yum install -y perl-libwww-perl perl-Compress-Zlib perl-Digest-SHA1 perl-Net* rsync perl-LockFile-Simple perl-Digest-MD5 bzip2
+yum install -y perl-libwww-perl perl-Compress-Zlib perl-Digest-SHA1 perl-Net* rsync perl-LockFile-Simple perl-Digest-MD5 bzip2 ed patch
 wget http://archive.ubuntu.com/ubuntu/pool/universe/d/debmirror/${tarfile}
 tar -xzvf ${tarfile}
 cd debmirror-${debmirror_version}ubuntu1
